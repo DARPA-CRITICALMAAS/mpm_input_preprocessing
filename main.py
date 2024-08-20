c775308756e70031f620189836a74d6b97997974
Justin Gawrilow <justin.gawrilow@gmail.com>
2024-08-20 13:51:26 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,2 @@
+# This file should have one main function which takes this as input: https://github.com/DARPA-CRITICALMAAS/cdr_schemas/blob/main/cdr_schemas/prospectivity_input.py#L128
+# and produces this: https://github.com/DARPA-CRITICALMAAS/cdr_schemas/blob/main/cdr_schemas/prospectivity_input.py#L112 as output for each input layer including the raster product.
